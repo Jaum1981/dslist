40ae8746c9311dc2d7136681f842333b15cb9b00
João Victor <juaun1981@alu.ufc.br>
2024-11-12 16:20:52 -0300

    configuração do properties, e ORM de Game

---
+++ src/main/java/com/jaum1981/dslist/entities/Game.java
@@ -0,0 +1,40 @@
+package com.jaum1981.dslist.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year") //year é uma palavra reservada no BD
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Game() {
+
+    }
+
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+
+
+}
